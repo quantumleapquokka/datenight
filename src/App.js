ae8db75b5bd19b36bbd00b5155caa7dbf8735536
Samantha Siew <sammie.siew@gmail.com>
2025-11-04 04:12:02 -0800

    main page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import background from './assets/background.jpg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+        className='h-screen w-screen flex items-center justify-center bg-cover bg-center'
+        style={{ backgroundImage: `url(${background})`}}
+    >
+        <div className='w-[600px] h-[400px] bg-jordy-blue rounded-lg shadow-lg flex flex-col items-center justify-center border-2 border-white'>
+            <p className='text-white font-bold text-4xl text-center leading-loose'>
+                hi pookie ૮ ˶ᵔ ᵕ ᵔ˶ ა
+                <br />
+                ˚*•̩̩͙✩•̩̩͙*˚＊˚*•̩̩͙✩•̩̩͙*˚＊
+                <br />
+                are you ready?
+            </p>
+            <p className='text-white font-serif text-sm-bold'>
+                p.s. you have to look at this on a computer + sound up ↻(𓄼 .̀  ̮.́)Ψ
+            </p>
+            <br />
+            <button className='bg-plum-web text-white font-bold py-2 px-4 rounded-lg border-2 border-white'>
+                i'm ready {'<'}3
+            </button>
+        </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'sky-magenta': '#e574bcff',
+            'persian-pink': '#ea84c9ff',
+            'plum-web': '#ef94d5ff',
+            'plum-web-2': '#f9b4edff',
+            'mauve': '#eabaf6ff',
+            'mauve-2': '#dabfffff',
+            'periwinkle': '#c4c7ffff',
+            'jordy-blue': '#adcfffff',
+            'light-sky-blue': '#96d7ffff',
+            'pale-azure': '#7fdeffff',
+        },
+    },
   },
   plugins: [],
 }
